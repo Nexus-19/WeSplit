6ec2635f1a76df691116fcac4ddea2318daae64c
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-06-17 20:04:21 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Soumyattam Dey on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
